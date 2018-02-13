ea57f0f88732acb57d88c69ca75d29965738d29c
realeve <realeve@qq.com>
2018-02-13 18:08:46 +0800

    细节调整

---
+++ src/Components/ComponentFormat.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class ComponentFormat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    };
+  }
+
+  render() {
+    return <div>test info</div>;
+  }
+}
+
+export default ComponentFormat;
